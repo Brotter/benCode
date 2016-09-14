5add7b730f69755f8aa18b05a8fc012d7a695715
Ben Rotter <brotter@dhcp-168-105-26-194.wireless.manoa.hawaii.edu>
2016-09-13 18:38:02 -1000

    fix entry output

--- waisPulses/waisNewCorrelator.cc
+++ waisPulses/waisNewCorrelator.cc
@@ -185,7 +185,7 @@ int main(int argc, char** argv) {
 
   for (int entry=startEntry; entry<stopEntry; entry++) {
     if (entry%10 == 0) {
-      cout << entry << " / " << stopEntry-startEntry << "\r";
+      cout << entry-startEntry << " / " << stopEntry-startEntry << "\r";
       fflush(stdout);
     }
 
