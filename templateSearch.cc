db2eee53afd1958d6306af0b3c09f25f86e31220
Ben Rotter <BenJRotter@gmail.com>
2017-03-28 17:55:20 -1000

    individual to single

--- templateSearch.cc
+++ templateSearch.cc
@@ -120,8 +120,8 @@ int main(int argc, char** argv) {
   //and a configuration for the analysis
   UCorrelator::AnalysisConfig *config = new UCorrelator::AnalysisConfig(); 
   //set the response to my "single" response
-  config->response_option = UCorrelator::AnalysisConfig::ResponseOption_t::ResponseIndividualBRotter;
-  //config.response_option = UCorrelator::AnalysisConfig::ResponseOption_t::ResponseSingleBRotter;
+  //  config->response_option = UCorrelator::AnalysisConfig::ResponseOption_t::ResponseIndividualBRotter;
+  config.response_option = UCorrelator::AnalysisConfig::ResponseOption_t::ResponseSingleBRotter;
 
   //and create an analyzer object
   UCorrelator::Analyzer *analyzer = new UCorrelator::Analyzer(config,true); ;
