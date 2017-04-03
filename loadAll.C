b48647e405ffdec29fbc7e2c3bd090ce4d6dd2ef
Ben Rotter <BenJRotter@gmail.com>
2017-04-03 10:56:18 -1000

    added a way to load things

---
+++ loadAll.C
@@ -0,0 +1,18 @@
+
+
+void loadAll(string date = "03.28.17_18h"){
+
+  TChain *summaryTree = new TChain("summaryTree","summaryTree");
+  
+
+  stringstream name;
+  for (int run=130; run<213; run++) {
+    name.str("");
+    name << "/home/brotter/nfsShared/results/tempateSearch/" << date << "/" << run << ".root";
+    summaryTree->Add(name.str().c_str());
+
+  }
+
+  return;
+
+}
