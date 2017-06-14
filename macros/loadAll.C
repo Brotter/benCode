34d84492622eed34cfb88d490e3c8d740e25158c
Ben Rotter <BenJRotter@gmail.com>
2017-06-14 11:41:53 -1000

    changed base dir to an environment variable

--- macros/loadAll.C
+++ macros/loadAll.C
@@ -6,12 +6,14 @@ void loadAll(string date = "06.07.17_17h"){
   
   gROOT->ProcessLine(".x setupProof.C");
 
+  char* resultsDir = getenv("ANITA3_RESULTSDIR");
+
   stringstream name;
   for (int run=130; run<433; run++) {
     if ( (run==130) || (run==144) || (run==150) || (run==186) || (run==198) )continue;
     name.str("");
-    //    name << "/home/brotter/nfsShared/results/templateSearch/" << date << "/" << run << ".root";
-    name << "/Volumes/ANITA3data/bigAnalysisFiles/templateSearch/" << date << "/" << run << ".root";
+    name << resultsDir << date << "/" << run << ".root";
+
     summaryTree->Add(name.str().c_str());
 
   }
