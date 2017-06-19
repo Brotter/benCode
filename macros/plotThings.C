7b0e5dd2cec326b1b046dac0d96865f01e242296
Ben Rotter <BenJRotter@gmail.com>
2017-06-19 00:21:46 -1000

    histogram binning

--- macros/plotThings.C
+++ macros/plotThings.C
@@ -50,12 +50,12 @@ void plotThings() {
 
 
 
-  TH1D *bothCutsWais = new TH1D("bothCutsWais","bothCutsWais",10000,0,9e7);
+  TH1D *bothCutsWais = new TH1D("bothCutsWais","bothCutsWais",1000,0,9e7);
 
   summaryTree->Draw("eventNumber >> bothCutsWais","flags.pulser == 1 && TMath::Abs(TMath::RadToDeg()*TMath::ATan(coherent[0][0].U/coherent[0][0].Q)/2) < 15 && TMath::Sqrt(pow(coherent[0][0].Q,2)+pow(coherent[0][0].U,2))/coherent[0][0].I > 0.4 && templateCRayH[5] > 0.5 && peak[0][0].value > 0.05 ","colz");
 
 
-  TH1D *bothCuts = new TH1D("bothCuts","bothCuts",10000,0,9e7);
+  TH1D *bothCuts = new TH1D("bothCuts","bothCuts",1000,0,9e7);
 
   summaryTree->Draw("eventNumber >> bothCuts","flags.pulser == 0 && TMath::Abs(TMath::RadToDeg()*TMath::ATan(coherent[0][0].U/coherent[0][0].Q)/2) < 15 && TMath::Sqrt(pow(coherent[0][0].Q,2)+pow(coherent[0][0].U,2))/coherent[0][0].I > 0.4 && templateCRayH[5] > 0.5 && peak[0][0].value > 0.05 ","colz");
 
