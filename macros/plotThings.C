c21eb6e8d8ed600a0bab8527db08a31187cb4053
Ben Rotter <BenJRotter@gmail.com>
2017-06-18 23:30:06 -1000

    should be by event number

--- macros/plotThings.C
+++ macros/plotThings.C
@@ -11,9 +11,10 @@ void plotThings() {
   TH2D *ldb = new TH2D("ldb","ldb",500,0,1,500,0,1);
 
 
+  TCanvas *cTemplate = new TCanvas("cTemplate","cTemplate",800,600);
   summaryTree->Draw("templateCRayH[5]:peak[0][0].value >> noise","flags.pulser == 0","colz");
-  summaryTree->Draw("templateCRayH[5]:peak[0][0].value >> wais","flags.pulser == 1","colz");
-  summaryTree->Draw("templateCRayH[5]:peak[0][0].value >> ldb","flags.pulser == 2","colz");
+  summaryTree->Draw("templateCRayH[5]:peak[0][0].value >> wais","flags.pulser == 1","colzSame");
+  summaryTree->Draw("templateCRayH[5]:peak[0][0].value >> ldb","flags.pulser == 2","colzSame");
   
   
   TH2D *cut = new TH2D("cut","cut",500,0,1,500,0,1);
@@ -21,7 +22,7 @@ void plotThings() {
   summaryTree->Draw("templateCRayH[5]:peak[0][0].value >> ldb",
 		    "flags.pulser == 0 && templateCRayH[5] > 0.5 && peak[0][0].value > 0.05","colz");
 
-  
+  TCanvas *cPolariz = new TCanvas("cPolariz","cPolariz",800,600);
   TH2D *polariz = new TH2D("polariz","polairiz",500,0,1,360,-45,45);
   TH2D *polarizWais = new TH2D("polarizWais","polarizWais",500,0,1,360,-45,45);
   TH2D *polarizLDB = new TH2D("polarizLDB","polarizLDB",500,0,1,360,-45,45);
@@ -34,11 +35,11 @@ void plotThings() {
 
 
   summaryTree->Draw("TMath::RadToDeg()*TMath::ATan(coherent[0][0].U/coherent[0][0].Q)/2.:TMath::Sqrt(pow(coherent[0][0].Q,2)+pow(coherent[0][0].U,2))/coherent[0][0].I >> polarizWais"
-		    "flags.pulser == 1","colz");
+		    "flags.pulser == 1","colzSame");
 
 
   summaryTree->Draw("TMath::RadToDeg()*TMath::ATan(coherent[0][0].U/coherent[0][0].Q)/2.:TMath::Sqrt(pow(coherent[0][0].Q,2)+pow(coherent[0][0].U,2))/coherent[0][0].I >> polarizLDB"
-		    "flags.pulser == 2","colz");
+		    "flags.pulser == 2","colzSame");
 
 
 
@@ -48,9 +49,9 @@ void plotThings() {
 		    "flags.pulser == 2 && TMath::Abs(TMath::RadToDeg()*TMath::ATan(coherent[0][0].U/coherent[0][0].Q)/2) < 15 && TMath::Sqrt(pow(coherent[0][0].Q,2)+pow(coherent[0][0].U,2))/coherent[0][0].I > 0.4","colz");
 
 
-  TH1D *bothCuts = new TH1D("bothCuts","bothCuts",10000,0,4e6);
+  TH1D *bothCuts = new TH1D("bothCuts","bothCuts",10000,0,9e7);
 
-  summaryTree->Draw("Entry$ >> bothCuts","flags.pulser == 1 && TMath::Abs(TMath::RadToDeg()*TMath::ATan(coherent[0][0].U/coherent[0][0].Q)/2) < 15 && TMath::Sqrt(pow(coherent[0][0].Q,2)+pow(coherent[0][0].U,2))/coherent[0][0].I > 0.4 && templateCRayH[5] > 0.5 && peak[0][0].value > 0.05 ","colz");
+  summaryTree->Draw("eventNumber >> bothCuts","flags.pulser == 1 && TMath::Abs(TMath::RadToDeg()*TMath::ATan(coherent[0][0].U/coherent[0][0].Q)/2) < 15 && TMath::Sqrt(pow(coherent[0][0].Q,2)+pow(coherent[0][0].U,2))/coherent[0][0].I > 0.4 && templateCRayH[5] > 0.5 && peak[0][0].value > 0.05 ","colz");
 
 
 
