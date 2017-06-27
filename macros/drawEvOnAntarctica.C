f477142535506998492c16461aaf6854251cadda
Ben Rotter <BenJRotter@gmail.com>
2017-06-27 13:44:56 -0700

    okay maybe this will fix it

--- macros/drawEvOnAntarctica.C
+++ macros/drawEvOnAntarctica.C
@@ -118,9 +118,11 @@ TProfile2D* makeCutHist() {
 
   TProfile2D *cutLatLon = new TProfile2D("cutLatLon","cutLatLon",250,-90,-65,360,-180,180);
 
-  summaryTree->Draw("peak[0][0].longitude:peak[0][0].latitude:templateCRayH[5][0] >> cutLatLon",
+  summaryTree->Draw("peak[0][0].latitude:peak[0][0].longitude:templateCRayH[5][0] >> cutLatLon",
 		    "flags.pulser == 0 && peak[0][0].latitude > -999","colz");
 
+
+  cout << cutLatLon->GetEntries() << endl;
 
   return cutLatLon;
 }
