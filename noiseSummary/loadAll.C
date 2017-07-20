635611121d1d1cca200e91adc19ab1e301b41df7
Ben Rotter <BenJRotter@gmail.com>
2017-07-20 11:02:04 -1000

    added a way to load them all

---
+++ noiseSummary/loadAll.C
@@ -0,0 +1,49 @@
+
+
+TChain* loadAll(string date = "07.05.17_22h/"){
+  //crab
+  //kwaabz
+  //crab
+  //crabcrab
+  //kiki
+  //popo
+  //bert
+  //kona
+  //tate
+
+  TChain *summaryTree = new TChain("summaryTree");
+  
+  gROOT->ProcessLine(".x setupProof.C");
+
+  char* resultsDir = getenv("ANITA3_RESULTSDIR");
+
+  stringstream name;
+  for (int run=0; run<256; run++) {
+
+    name.str("");
+    name << resultsDir << "/noiseSummary/" << date << "/" << run << ".root";
+
+    summaryTree->Add(name.str().c_str());
+
+  }
+
+    summaryTree->SetProof();
+
+  return summaryTree;
+
+}
+
+
+TH1* makeCutStrengthPlot(TH1* inHist) {
+
+  TH1* copyHist = (TH1*)inHist->Clone();
+  
+  copyHist->Scale(1./copyHist->GetIntegral()[copyHist->GetNbinsX()]);
+
+  TH1* outHist = copyHist->GetCumulative();
+  delete copyHist;
+
+  return outHist;
+}
+
+  
