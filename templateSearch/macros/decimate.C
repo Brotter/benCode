b714efce36beea62045169c7840e9aeeefac80a6
Ben Rotter <BenJRotter@gmail.com>
2017-08-09 15:18:43 -1000

    added a thing that stupidly decimiates the summary

---
+++ templateSearch/macros/decimate.C
@@ -0,0 +1,67 @@
+/*
+
+  Going through the 100% dataset takes a really long time, so this is a method for decimating it I think
+
+  Super simple, just event numbers that end in zero
+
+ */
+
+
+
+
+
+
+
+void decimate(string date="07.28.17_17h") {
+
+
+
+  TChain *summaryTree = new TChain("summaryTree","summaryTree");
+  char* resultsDir = getenv("ANITA3_RESULTSDIR");
+  stringstream name;
+  for (int run=0; run<256; run++) {
+    name.str("");
+    name << resultsDir << "templateSearch/" << date << "/" << run << ".root";
+    summaryTree->Add(name.str().c_str());
+  }
+
+  
+  TFile *outFile = new TFile(TString(date)+"_decimated.root","recreate");
+  TTree *outTree = new TTree("summaryTree","summaryTree");
+
+  AnitaEventSummary *evSummary = NULL;
+  AnitaTemplateSummary *tmpSummary = NULL;
+  AnitaNoiseSummary *noiseSummary = NULL;
+  Adu5Pat *gps = NULL;
+
+  summaryTree->SetBranchAddress("eventSummary",&evSummary);
+  summaryTree->SetBranchAddress("template",&tmpSummary);
+  summaryTree->SetBranchAddress("noiseSummary",&noiseSummary);
+  summaryTree->SetBranchAddress("gpsEvent",&gps);
+
+  outTree->Branch("eventSummary",&evSummary);
+  outTree->Branch("template",&tmpSummary);
+  outTree->Branch("noiseSummary",&noiseSummary);
+  outTree->Branch("gpsEvent",&gps);
+
+  int lenEntries = summaryTree->GetEntries();
+
+  cout << "found " << lenEntries << " entries" << endl;
+
+  for (int entry=0; entry<lenEntries; entry++) {
+    summaryTree->GetEntry(entry);
+    
+    if (entry%10000 == 0) cout << entry << "/" << lenEntries << endl;
+
+    if (entry%10 == 0) {
+      outTree->Fill();
+    }
+
+  }
+
+  outTree->Write();
+  outFile->Close();
+
+  return;
+
+}
