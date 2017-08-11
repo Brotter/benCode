db86df52a75858110728be1bca864d6792a1db7a
Ben Rotter <BenJRotter@gmail.com>
2017-08-10 16:27:23 -1000

    split plotThingsScan into another macro and made it have the decimated set as an option instead of a requirement

--- templateSearch/macros/plotThingsScan.C
+++ templateSearch/macros/plotThingsScan.C
@@ -1,3 +1,12 @@
+/*
+
+
+  Making histograms takes awhile, and PROOF keeps dropping random things, so lets make them all here!
+
+
+ */
+
+
 void makeReducedHistograms(TH1D **histograms,string subName) {
 
   //PointingHypothesis
@@ -149,7 +158,7 @@ void fillHistograms(AnitaEventSummary *eventSummary, AnitaTemplateSummary *templ
 
 }
 
-void plotThingsScan() {
+void plotThingsScan(bool decimated=true) {
 
   /*
     Proof is stupid and keeps dropping huge amounts of events for some reason.
@@ -158,10 +167,13 @@ void plotThingsScan() {
 
    */
 
-  //  TChain *summaryTree = (TChain*)gROOT->ProcessLine(".x loadAll.C");
 
+  if (decimated) {
   TChain *summaryTree = new TChain("summaryTree","summaryTree");
   summaryTree->Add("07.28.17_17h_decimated.root");
+  } else {
+    TChain *summaryTree = (TChain*)gROOT->ProcessLine(".x loadAll.C");
+  }
 
   const int numHists = 31;
 
@@ -255,139 +267,4 @@ void plotThingsScan() {
   outFile->Close();
 
   return;
-}
-
-
-TH1* makeNormCumulative(TH1* inHist) {
-  /*
-    Makes a "normalized cumulative cut fraction" graph I think
-
-    Should let me set cuts on a specific amount of "reduction"
-   */
-
-
-  TH1* copyHist = (TH1*)inHist->Clone();
-  
-  double integral = 0;
-  for (int i=0; i<copyHist->GetNbinsX(); i++) {
-    double value = copyHist->GetBinContent(i);
-    integral += value;
-  }
-
-  copyHist->Scale(1./integral);
-  TH1* outHist = (TH1*)copyHist->GetCumulative();
-
-  for (int i=0; i<copyHist->GetNbinsX(); i++) {
-    double value = outHist->GetBinContent(i);
-    outHist->SetBinContent(i,1.-value);
-  }
-
-
-  delete copyHist;
-
-  outHist->GetYaxis()->SetTitle("Surviving Fraction");
-  return outHist;
-}
-
-
-TH1* makeNormHist(TH1* inHist) {
-  /*
-
-    Make a normalized histogram
-
-  */
-
-  TH1* copyHist = (TH1*)inHist->Clone();
-  copyHist->GetYaxis()->SetTitle("Occupancy");
-
-  
-  double integral = 0;
-  for (int i=0; i<copyHist->GetNbinsX(); i++) {
-    double value = copyHist->GetBinContent(i);
-    integral += value;
-  }
-
-  copyHist->Scale(1./integral);
-
-  return copyHist;
-}
-
-void drawOneDHistos(string inFileName="plotThingsScan.root") {
-  
-  stringstream name;
-
-  TFile *inFile = TFile::Open(inFileName.c_str());
-
-  const int numLines = 5;
-  string subStrings[numLines] = {"thermal","WAIS","LDB","basePointed","minbias"};
-
-  TCanvas *c1 = new TCanvas("c1","c1",1000,600);
-
-
-  const int numGraphs = 31;
-  string graphNames[numGraphs] = {"peakPhi","peakTheta","mapPeak","mapSNR","peakRatio",
-			   "linPolFrac","linPolAng","waveSNR","wavePeakVal","wavePeakHilb","impulsivity",
-			   "linPolFrac_F","linPolAng_F","waveSNR_F","wavePeakVal_F","wavePeakHilb_F","impulsivity_F",
-			   "linPolFrac_D","linPolAng_D","waveSNR_D","wavePeakVal_D","wavePeakHilb_D","impulsivity_D",
-			   "linPolFrac_DF","linPolAng_DF","waveSNR_DF","wavePeakVal_DF","wavePeakHilb_DF","impulsivity_DF",
-			   "template_Wais","template_cRay"};
-
-  for (int graph=0; graph<numGraphs; graph++) {
-    c1->Clear();
-    c1->SetGrid(1);
-    c1->SetLogy();
-    TLegend *leg = new TLegend(0.8,0.7,0.95,0.95);
-    for (int i=0; i<numLines; i++) {
-      name.str("");
-      name << graphNames[graph] << "_" << subStrings[i];
-      TH1 *hist = makeNormCumulative((TH1D*)inFile->Get(name.str().c_str()));
-      hist->SetLineColor(i+1);
-      hist->GetYaxis()->SetRangeUser(1e-7,1);
-      hist->SetStats(0);
-      if (i==0) hist->Draw();
-      else      hist->Draw("same");
-      if (strcmp(subStrings[i].c_str(),"thermal")==0) leg->AddEntry(hist,"events","l");
-      else leg->AddEntry(hist,subStrings[i].c_str(),"l");
-    }
-
-    leg->Draw();
-    c1->SaveAs(TString("plots/")+graphNames[graph]+TString("_cumulative.png"));
-  }
-
-
-  for (int graph=0; graph<numGraphs; graph++) {
-    c1->Clear();
-    c1->SetGrid(1);
-    c1->SetLogy();
-    TLegend *leg = new TLegend(0.8,0.7,0.95,0.95);
-    for (int i=0; i<numLines; i++) {
-      name.str("");
-      name << graphNames[graph] << "_" << subStrings[i];
-      TH1 *hist = makeNormHist((TH1D*)inFile->Get(name.str().c_str()));
-      hist->SetLineColor(i+1);
-      hist->GetYaxis()->SetRangeUser(1e-8,1);
-      hist->SetStats(0);
-      if (i==0) hist->Draw();
-      else      hist->Draw("same");
-      if (strcmp(subStrings[i].c_str(),"thermal")==0) leg->AddEntry(hist,"events","l");
-      else leg->AddEntry(hist,subStrings[i].c_str(),"l");
-    }
-
-    leg->Draw();
-    c1->SaveAs(TString("plots/")+graphNames[graph]+TString("_hist.png"));
-  }
-
-  return;
-}
-  
-
-TH1 *dividedCumulative(TH1D *h1, TH1D *h2) {
-
-  TH1* h1Cum = makeNormCumulative(h1);
-  TH1* h2Cum = makeNormCumulative(h2);
-
-  h1Cum->Divide(h2Cum);
-  delete h2Cum;
-  
-  return h1Cum;
 }
