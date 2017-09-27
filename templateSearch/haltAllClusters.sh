494736208179f3d1b68674b073bfc26763889ccf
Ben Rotter <BenJRotter@gmail.com>
2017-09-27 08:02:24 -1000

    new ways to control all the clusters

---
+++ templateSearch/haltAllClusters.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+#
+#
+# For when I mess up and have to stop everything
+#
+#
+#
+
+
+ssh anitai   'killall -SIGQUIT templateSearch'
+ssh anitaii  'killall -SIGQUIT templateSearch'
+ssh anitaiii 'killall -SIGQUIT templateSearch'
+ssh anitaiv  'killall -SIGQUIT templateSearch'
+
+
---
+++ templateSearch/pauseAllClusters.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+#
+#
+# For when I mess up and have to stop everything
+#
+#
+#
+
+
+ssh anitai   'killall -SIGTERM templateSearch'
+ssh anitaii  'killall -SIGTERM templateSearch'
+ssh anitaiii 'killall -SIGTERM templateSearch'
+ssh anitaiv  'killall -SIGTERM templateSearch'
+
+
