bfd60515d02cf0641a4077e56042c7388d194697
Ben Rotter <BenJRotter@gmail.com>
2017-09-27 14:29:54 -1000

    added a way to update all the servers

---
+++ templateSearch/updateClusterTools.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+#
+#
+# So that I don't have to log into each server and update it individually.
+#
+# Be careful because this could totally fail
+#
+
+
+ssh anitai 'cd ~/anita16/anitaBuildTool; ./updateComponents 0; make; make install' &
+ssh anitaii 'cd ~/anita16/anitaBuildTool; ./updateComponents 0; make; make install' &
+ssh anitaiii 'cd ~/anita16/anitaBuildTool; ./updateComponents 0; make; make install' &
+ssh anitaiv 'cd ~/anita16/anitaBuildTool; ./updateComponents 0; make; make install' &
+
+
