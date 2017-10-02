90f4d5c314d38d8b59b82334ceeb70f5897e3843
Ben Rotter <BenJRotter@gmail.com>
2017-10-02 07:37:18 -1000

    added a script to combine the redo runs

---
+++ templateSearch/macros/combineRedos.C
@@ -0,0 +1,36 @@
+/*
+
+  The redo runs are split into 12, this should combine them
+
+ */
+
+
+
+void combineRedos(int core) {
+
+  TChain *summaryTree = new TChain("summaryTree");
+
+  stringstream name;
+  name.str("");
+  char* homeDir = getenv("HOME");
+  string dataDir = "/anita16/benCode/templateSearch/09.27.17_19h/";
+  for (int i=0; i<12; i++) {
+    name << homeDir << dataDir << core << "_" << i+1 << ".root";
+    summaryTree->Add(name.str().c_str());
+  }
+
+  name.str("");
+  name << homeDir << dataDir << core << ".root";
+  TFile *outFile = TFile::Open(name.str().c_str(),"recreate");
+  summaryTree->CloneTree(-1,"fast");
+  outFile->Write();
+  outFile->Close();
+
+
+  return;
+}
+
+void combineRedos() {
+  cout << "combineRedos(int core): You didn't give me a core number!  Loading instead..." << endl;
+  return;
+}
