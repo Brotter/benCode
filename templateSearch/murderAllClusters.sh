85713c4592c14bdac279aa5cedfeb6ca3ada21e9
Ben Rotter <BenJRotter@gmail.com>
2017-10-02 00:22:14 -1000

    a fun new murdering cluster tool

---
+++ templateSearch/murderAllClusters.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+#
+#
+# Sometimes things just don't get the hint with a nice quit, so then you have to really kill them
+#
+#
+#
+
+
+ssh anitai   'killall -9 templateSearch'
+ssh anitaii  'killall -9 templateSearch'
+ssh anitaiii 'killall -9 templateSearch'
+ssh anitaiv  'killall -9 templateSearch'
+
+
