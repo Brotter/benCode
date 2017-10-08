02f5de0e1e3a556b7c772d9a8d37b335bc939fb1
Ben Rotter <BenJRotter@gmail.com>
2017-10-08 11:43:16 -1000

    Fixed to match other control scripts

--- templateSearch/macros/runBackgroundCluster.sh
+++ templateSearch/macros/runBackgroundCluster.sh
@@ -10,25 +10,12 @@
 #################################################
 
 startTime=`date +%m.%d.%y_%Hh`
-sharedDir="/home/brotter/nfsShared/results/cluster/"${startTime}
+sharedDir="clusteringOutputs/"${startTime}
 mkdir ${sharedDir}
 mkdir ${sharedDir}"/log"
 
-numCores=192
-if [ `hostname | cut -d"." -f1` == "anitaI" ]; then
-    startCore=0
-elif [ `hostname | cut -d"." -f1` == "anitaII" ]; then
-    startCore=64
-elif [ `hostname | cut -d"." -f1` == "anitaIII" ]; then
-    startCore=64
-elif [ `hostname | cut -d"." -f1` == "anitaIV" ]; then
-    startCore=128
-else
-    echo "The server isn't an anita cluster server, so you shouldn't use this script"	
-    exit
-fi
-
-for localCore in `seq ${startCore} $((startCore+63))`; do
+numCores=64
+for localCore in `seq 0 63`; do
     echo "Starting "${localCore}
-    root -b cluster.C\(${numCores},${localCore},\"${sharedDir}\"\)  1> ${sharedDir}/log/${localCore}.log 2>&1 &
+    root -b cluster.C\(\"runBackgroundCluster\",${numCores},${localCore},\"${sharedDir}\"\)  1> ${sharedDir}/log/${localCore}.log 2>&1 &
 done
