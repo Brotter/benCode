87ea61a07e8787e1121e795899c3f67ce3d39575
Ben Rotter <BenJRotter@gmail.com>
2017-10-08 11:41:59 -1000

    added some new control scripts that I'll move somewhere else eventually

---
+++ templateSearch/macros/runEventCluster.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+################################################
+#
+#  Ben Rotter - September 2016
+#
+#  This code should run on the four ANITA clusters
+#  at UH to run whatever code faster
+#
+#################################################
+
+startTime=`date +%m.%d.%y_%Hh`
+sharedDir="clusteringOutputs/"${startTime}
+mkdir ${sharedDir}
+mkdir ${sharedDir}"/log"
+
+numCores=64
+for localCore in `seq 0 64`; do
+    echo "Starting "${localCore}
+    root -b cluster.C\(\"runClusterEvents\",${numCores},${localCore},\"${sharedDir}\"\)  1> ${sharedDir}/log/${localCore}.log 2>&1 &
+done
