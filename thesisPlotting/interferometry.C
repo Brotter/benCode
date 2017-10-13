5823057e0aaf6d89fc1cd9fd3efe30d03f7b8ecd
Ben Rotter <BenJRotter@gmail.com>
2017-10-13 04:53:16 -1000

    code to make the a nice interferometry example plot

---
+++ thesisPlotting/interferometry.C
@@ -0,0 +1,75 @@
+/*
+
+  using Correlator.cc and setting a bunch of disallowed antennas to make example maps
+
+ */
+#include "AnitaDataset.h"
+#include "Analyzer.h"
+#include "AntennaPositions.h"
+#include "UCFilters.h"
+
+void interferometry() {
+
+  AnitaDataset *data = new AnitaDataset(342);
+  data->getEvent(58023977);
+
+  FilterStrategy *fStrat = UCorrelator::getStrategyWithKey("sinsub_10_3_ad_2");
+
+  FilteredAnitaEvent *filtered = new FilteredAnitaEvent(data->useful(), fStrat, data->gps(), data->header());
+
+
+  UCorrelator::Correlator *corr = new UCorrelator::Correlator(140,40,180,41,-20,20);
+
+
+  const UCorrelator::AntennaPositions * ap = UCorrelator::AntennaPositions::instance(); 
+
+  int closest[15];
+  int nant = ap->getClosestAntennas(110, 15, closest, 0, AnitaPol::kHorizontal);
+
+  for (int i=0; i<15; i++) {
+    cout << closest[i] << endl; }
+
+
+  ULong64_t allowed = 0;  
+  TH2D* hists[6];
+  //set two of the antennas to 1 iteratively,
+  //0,1 0,2 0,3 1,2 1,3 2,3 
+  int cnt=0;
+  for (int i=0; i<4; i++) {
+    for (int j=i+1; j<4; j++) {
+      cout << closest[i] << "," << closest[j] << endl;
+      allowed = 0;
+      allowed |= (1ul << closest[i]);
+      allowed |= (1ul << closest[j]);
+      corr->setAllowedAntennas(allowed);
+      corr->compute(filtered,AnitaPol::kHorizontal);
+      hists[cnt] = new TH2D(*corr->getHist());
+      cnt++;
+    }
+  }
+  allowed=0;
+  for (int i=0; i<4; i++) {
+    allowed |= (1ul << closest[i]);
+  }
+  cout << allowed << endl;
+  corr->setAllowedAntennas(allowed);
+  corr->compute(filtered,AnitaPol::kHorizontal);
+  TH2D *histTot = new TH2D(*corr->getHist());
+    
+  TCanvas *c1 = new TCanvas("c1","c1",1000,600);
+  TPad *pad1 = new TPad("pad1", "The pad 80% of the height",0.0,0.0, 0.8,1.0);
+  pad1->Divide(3,2);
+  TPad *pad2 = new TPad("pad2", "The pad 80% of the height",0.8,0.0, 1.0,1.0);
+  pad1->Draw();
+  pad2->Draw();
+  for (int i=0; i<6; i++) {
+    pad1->cd(i+1);
+    hists[i]->Draw("colz");
+  }
+  pad2->cd();
+  histTot->Draw("colz");
+
+  
+
+}
+  
