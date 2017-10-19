b775fd37026690fdc0ae3aa896bc1601f6bff222
Ben Rotter <BenJRotter@gmail.com>
2017-10-18 14:37:56 -1000

    added more values for making a spreadsheet of passing event quantities

--- templateSearch/macros/printNotable.C
+++ templateSearch/macros/printNotable.C
@@ -6,7 +6,7 @@
 #include "AnitaEventSummary.h"
 
 
-void printNotable(string fileNameBase="candidates_oct13") {
+void printNotable(string fileNameBase="cutsClust_oct14",double clusterThreshold=40) {
 
   stringstream name;
   name.str("");
@@ -22,27 +22,37 @@ void printNotable(string fileNameBase="candidates_oct13") {
   summaryTree->SetBranchAddress("eventSummary",&evSum);
   AnitaTemplateSummary *tempSum = NULL;
   summaryTree->SetBranchAddress("template",&tempSum);
+  double clusterValue;
+  summaryTree->SetBranchAddress("clusterValue",&clusterValue);
+  clusterValue = 999;
 
   name.str("");
-  name << fileNameBase << "_values.txt";
+  name << fileNameBase << "_values.csv";
   ofstream outfile(name.str());
 
-  outfile << "candidateNum eventNumber templateCorr mapPeak mapSNR mapTheta cohLinPolFrac deconvLinPolFrac cohTotPolFrac deconvTotPolFrac cohHilbPeak deconvHilbPeak" << endl;
+  outfile << "candidateNum eventNumber coherTemplateCorr deconvTemplateCorr mapPeak mapSNR mapTheta cohLinPolFrac deconvLinPolFrac cohTotPolFrac deconvTotPolFrac cohHilbPeak deconvHilbPeak" << endl;
   
+  int candNum=0;
   for (int i=0; i<lenEntries; i++) {
     summaryTree->GetEntry(i);
     
-    AnitaEventSummary::WaveformInfo cohrnt = evSum->coherent_filtered[0][0];
-    AnitaEventSummary::WaveformInfo deconv = evSum->deconvolved_filtered[0][0];
+    if ( (clusterThreshold>0 && (clusterValue > clusterThreshold)) || 
+	  (evSum->eventNumber==27142546 || evSum->eventNumber==39599205) ) {
 
-    outfile << i << " " << evSum->eventNumber << " " << tempSum->coherent[0][0].cRay[4] << " ";
-    outfile << evSum->peak[0][0].value << " " << evSum->peak[0][0].snr << " " << evSum->peak[0][0].theta << " ";
-    outfile << cohrnt.linearPolFrac() << " " << deconv.linearPolFrac() << " ";
-    outfile << cohrnt.totalPolFrac() << " " << deconv.totalPolFrac() << " ";
-    outfile << cohrnt.peakHilbert << " " << deconv.peakHilbert << endl;
-
+      AnitaEventSummary::WaveformInfo cohrnt = evSum->coherent_filtered[0][0];
+      AnitaEventSummary::WaveformInfo deconv = evSum->deconvolved_filtered[0][0];
+    
+      outfile << candNum << " " << evSum->eventNumber << " ";
+      outfile << tempSum->coherent[0][0].cRay[4] << " " << tempSum->deconvolved[0][0].cRay[4] << " ";
+      outfile << evSum->peak[0][0].value << " " << evSum->peak[0][0].snr << " " << evSum->peak[0][0].theta << " ";
+      outfile << cohrnt.linearPolFrac() << " " << deconv.linearPolFrac() << " ";
+      outfile << cohrnt.totalPolFrac() << " " << deconv.totalPolFrac() << " ";
+      outfile << cohrnt.peakHilbert << " " << deconv.peakHilbert << endl;
+      candNum++;
+    }
   }
-
+  cout << "Found " << candNum << " within threshold" << endl;
+  
   outfile.close();
 
 
