9c6e7e2ed73263ec3b9e1b84be80f7ac14fb6451
Ben Rotter <BenJRotter@gmail.com>
2017-11-04 15:57:17 -1000

    a way to run newStokes.C on the cluster

---
+++ templateSearch/macros/runNewStokesWais.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+################################################
+#
+#  Ben Rotter - September 2016
+#
+#  This code should run on the four ANITA clusters
+#  at UH to run whatever code faster
+#
+#################################################
+
+startTime=`date +%m.%d.%y_%Hh%Mm`
+echo ${startTime}
+sharedDir="waisEvents_newStokes/"
+mkdir ${sharedDir}
+mkdir ${sharedDir}"/log"
+
+
+numCores=64
+for localCore in `seq 0 64`; do
+    echo "Starting "${localCore}" to "${sharedDir}
+    root -b newStokes.C\(\"waisEvents.root\",64,${localCore},\"${sharedDir}\"\)  1> ${sharedDir}/log/${localCore}.log 2>&1 &
+done
