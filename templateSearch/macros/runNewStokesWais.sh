8217b98ff16c4b5c3754601783d36303c63b4893
Ben Rotter <BenJRotter@gmail.com>
2017-11-14 21:14:11 -1000

    newStokes is different

--- templateSearch/macros/runNewStokesWais.sh
+++ templateSearch/macros/runNewStokesWais.sh
@@ -19,5 +19,5 @@ mkdir ${sharedDir}"/log"
 numCores=64
 for localCore in `seq 0 64`; do
     echo "Starting "${localCore}" to "${sharedDir}
-    root -b newStokes.C\(\"waisEvents.root\",64,${localCore},\"${sharedDir}\"\)  1> ${sharedDir}/log/${localCore}.log 2>&1 &
+    root -b newStokes.C\(\"waisEvents.root\",64,${localCore},true,\"${sharedDir}\"\)  1> ${sharedDir}/log/${localCore}.log 2>&1 &
 done
