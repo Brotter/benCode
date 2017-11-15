3260c0ed965386516f69b603ee7996d8c35a6c9e
Ben Rotter <BenJRotter@gmail.com>
2017-11-14 21:15:11 -1000

    print out stuff for MATLAB as a default

--- templateSearch/macros/printNotable.C
+++ templateSearch/macros/printNotable.C
@@ -6,7 +6,7 @@
 #include "AnitaEventSummary.h"
 
 
-void printNotable(string fileNameBase="cutsClust_oct14",double clusterThreshold=40) {
+void printNotable(string fileNameBase="cutsClust_oct14_geo_labeled",double clusterThreshold=40) {
 
   stringstream name;
   name.str("");
@@ -25,38 +25,47 @@ void printNotable(string fileNameBase="cutsClust_oct14",double clusterThreshold=
   double clusterValue;
   summaryTree->SetBranchAddress("clusterValue",&clusterValue);
   clusterValue = 999;
+  TString *labelString = NULL;
+  summaryTree->SetBranchAddress("label",&labelString);
+
 
   name.str("");
   name << fileNameBase << "_values.csv";
   ofstream outfile(name.str());
-
-  outfile << "candidateNum eventNumber sourceLat sourceLon sourceAlt anitaLat anitaLon anitaAlt elevation trueAz coherTemplateCorr deconvTemplateCorr mapPeak mapSNR mapTheta cohLinPolFrac deconvLinPolFrac cohTotPolFrac deconvTotPolFrac cohHilbPeak deconvHilbPeak" << endl;
   
+  //  outfile << "candidateNum eventNumber realTime sourceLat sourceLon sourceAlt anitaLat anitaLon anitaAlt elevation trueAz coherTemplateCorr deconvTemplateCorr mapPeak mapSNR mapTheta cohLinPolFrac deconvLinPolFrac cohTotPolFrac deconvTotPolFrac cohHilbPeak deconvHilbPeak" << endl;
+  outfile << "candidateNum eventNumber realTime sourceLat sourceLon sourceAlt anitaLat anitaLon anitaAlt elevation trueAz" << endl;
   int candNum=0;
   for (int i=0; i<lenEntries; i++) {
     summaryTree->GetEntry(i);
     
-    if ( (clusterThreshold>0 && (clusterValue > clusterThreshold)) || 
-	  (evSum->eventNumber==27142546 || evSum->eventNumber==39599205) ) {
+    //    if ( (clusterThreshold>0 && (clusterValue > clusterThreshold)) || 
+    //	  (evSum->eventNumber==27142546 || evSum->eventNumber==39599205) ) {
 
-      AnitaEventSummary::WaveformInfo cohrnt = evSum->coherent_filtered[0][0];
-      AnitaEventSummary::WaveformInfo deconv = evSum->deconvolved_filtered[0][0];
-      
-      AnitaEventSummary::PointingHypothesis peak = evSum->peak[0][0];
-      AnitaEventSummary::PayloadLocation anita = evSum->anitaLocation;
+    if (!(strstr(labelString->Data(),"Clustered Passing"))) continue;
+    
 
+    AnitaEventSummary::WaveformInfo cohrnt = evSum->coherent_filtered[0][0];
+    AnitaEventSummary::WaveformInfo deconv = evSum->deconvolved_filtered[0][0];
+    
+    AnitaEventSummary::PointingHypothesis peak = evSum->peak[0][0];
+    AnitaEventSummary::PayloadLocation anita = evSum->anitaLocation;
 
-      outfile << candNum << " " << evSum->eventNumber << " ";
-      outfile << peak.latitude << " " << peak.longitude << " " << peak.altitude << " ";
-      outfile << anita.latitude << " " << anita.longitude << " " << anita.altitude << " ";
-      outfile << peak.theta << " " << FFTtools::wrap(anita.heading-peak.phi,360,0) << " ";
-      outfile << tempSum->coherent[0][0].cRay[4] << " " << tempSum->deconvolved[0][0].cRay[4] << " ";
-      outfile << peak.value << " " << peak.snr << " " << peak.theta << " ";
-      outfile << cohrnt.linearPolFrac() << " " << deconv.linearPolFrac() << " ";
-      outfile << cohrnt.totalPolFrac() << " " << deconv.totalPolFrac() << " ";
-      outfile << cohrnt.peakHilbert << " " << deconv.peakHilbert << endl;
-      candNum++;
-    }
+    
+    outfile << candNum << " " << evSum->eventNumber << " " << evSum->realTime << " ";
+    outfile << peak.latitude << " " << peak.longitude << " " << peak.altitude << " ";
+    outfile << anita.latitude << " " << anita.longitude << " " << anita.altitude << " ";
+    outfile << peak.theta << " " << FFTtools::wrap(anita.heading-peak.phi,360,0)+180 << " "; //true azimuth. 0-360 CW from north
+    /*
+    outfile << tempSum->coherent[0][0].cRay[4] << " " << tempSum->deconvolved[0][0].cRay[4] << " ";
+    outfile << peak.value << " " << peak.snr << " " << peak.theta << " ";
+    outfile << cohrnt.linearPolFrac() << " " << deconv.linearPolFrac() << " ";
+    outfile << cohrnt.totalPolFrac() << " " << deconv.totalPolFrac() << " ";
+    outfile << cohrnt.peakHilbert << " " << deconv.peakHilbert;
+    */
+    outfile << endl;
+    candNum++;
+    
   }
   cout << "Found " << candNum << " within threshold" << endl;
   
